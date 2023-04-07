367265793be424943d935764f34fdc4866605846
Dyphnah Nyamongo <nyandukodyphnah4@gmail.com>
2023-04-07 11:11:35 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 =2;
+const num2 =31;
+let multiply = (num1 * num2);  
+console.log(2*31);
 
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 14;
+let num4 = 5;
+const mod = (num3 % num4);
+console.log(14%5);
+
+const max = Math.max( 3, 5, 9, 17, 19, 20);
 // Write your code here
